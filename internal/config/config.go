978a4abe8b2834a769ec47e839a66a7d8da5218f
Timur Enakaev <timur.enakaev.2000@gmail.com>
2025-04-19 19:56:06 +0300

    iter4: fix port

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,17 +4,17 @@ import "flag"
 
 type ConfigType struct {
 	Address      string
-	BaseShortUrl string
+	BaseShortURL string
 }
 
 var ServerConfig = ConfigType{
-	Address:      "localhost:8888",
-	BaseShortUrl: "http://localhost:8000/qsd54gFg",
+	Address:      "localhost:8080",
+	BaseShortURL: "http://localhost:8000/qsd54gFg",
 }
 
 func ParseFlags() {
 	flag.StringVar(&ServerConfig.Address, "a", "localhost:8888", "address to run server")
-	flag.StringVar(&ServerConfig.BaseShortUrl, "b", "http://localhost:8000/qsd54gFg", "address to run server")
+	flag.StringVar(&ServerConfig.BaseShortURL, "b", "http://localhost:8000/qsd54gFg", "address to run server")
 
 	flag.Parse()
 }
