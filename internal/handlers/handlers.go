0369e5f6cac8f392825d4acba9c6a467bb7c102a
Timur Enakaev <timur.enakaev.2000@gmail.com>
2025-04-19 20:12:16 +0300

    iter4: some fix

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -11,7 +11,6 @@ import (
 var Store = make(map[string]string)
 
 func PostMainHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hello"))
 	if r.Method != http.MethodPost {
 		http.Error(w, "Bad Request", http.StatusBadRequest)
 		return
@@ -29,8 +28,6 @@ func PostMainHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func GetIDHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("hello"))
-
 	if r.Method != http.MethodGet {
 		http.Error(w, "Bad Request", http.StatusBadRequest)
 		return
