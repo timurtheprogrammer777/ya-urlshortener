8d4221d2d95ac7a806594bd65ace7a9e61330ea5
Timur Enakaev <timur.enakaev.2000@gmail.com>
2025-04-19 20:21:40 +0300

    iter4: changed hardcode to config.b

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -6,6 +6,8 @@ import (
 	"io"
 	"net/http"
 	"strings"
+
+	"github.com/timurtheprogrammer777/ya-urlshortener.git/internal/config"
 )
 
 var Store = make(map[string]string)
@@ -24,7 +26,7 @@ func PostMainHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusCreated)
 	w.Header().Set("Content-Type", "text/plain")
-	w.Write([]byte("http://localhost:8080/" + id))
+	w.Write([]byte(config.ServerConfig.BaseShortURL + "/" + id))
 }
 
 func GetIDHandler(w http.ResponseWriter, r *http.Request) {
