40060a271f3739fa7eff8b89f2466170ffb626d8
Timur Enakaev <timur.enakaev.2000@gmail.com>
2025-04-19 19:58:24 +0300

    iter4: test

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,12 +8,12 @@ type ConfigType struct {
 }
 
 var ServerConfig = ConfigType{
-	Address:      "localhost:8080",
+	Address:      "localhost:8888",
 	BaseShortURL: "http://localhost:8000/qsd54gFg",
 }
 
 func ParseFlags() {
-	flag.StringVar(&ServerConfig.Address, "a", "localhost:8888", "address to run server")
+	flag.StringVar(&ServerConfig.Address, "a", "localhost:8080", "address to run server")
 	flag.StringVar(&ServerConfig.BaseShortURL, "b", "http://localhost:8000/qsd54gFg", "address to run server")
 
 	flag.Parse()
