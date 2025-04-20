35a5c20b5a6e5e6f91f4bf0396015d6bf37245c1
Timur Enakaev <timur.enakaev.2000@gmail.com>
2025-04-20 11:26:43 +0300

    iter4: test2

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -6,8 +6,6 @@ import (
 	"io"
 	"net/http"
 	"strings"
-
-	"github.com/timurtheprogrammer777/ya-urlshortener.git/internal/config"
 )
 
 var Store = make(map[string]string)
@@ -26,7 +24,8 @@ func PostMainHandler(w http.ResponseWriter, r *http.Request) {
 
 	w.WriteHeader(http.StatusCreated)
 	w.Header().Set("Content-Type", "text/plain")
-	w.Write([]byte(config.ServerConfig.BaseShortURL + "/" + id))
+	// w.Write([]byte(config.ServerConfig.BaseShortURL + "/" + id))
+	w.Write([]byte("http://localhost:8080/" + id))
 }
 
 func GetIDHandler(w http.ResponseWriter, r *http.Request) {
